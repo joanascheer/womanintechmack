a8a65ea5cd1b7967973745753fa452470a1f064a
Joana Scheer <scheer.joana@gmail.com>
2024-06-04 12:47:30 -0300

    injecting footer with js to clean the code

--- scripts.js
+++ scripts.js
@@ -1,19 +1,12 @@
 
-var userName = "Woman in Tech";
-var imagem = "images/fem.png";
-var currentYear = new Date().getFullYear();
-var link = "https://mackenzie.com.br";
-
-var coluna1 = document.getElementById('coluna1');
-coluna1.innerHTML = `<p>${userName} <img src="${imagem}" alt="sexo: Feminino"> * ${currentYear} | <a href="${link}">Mackenzie</a></p>`;
-
-var imagens = ["images/fb.png", "images/ig.png", "images/in.png"];
-var links = ["https://fcebook.com/primecatsgatil", "https://instagram.com/gudan_ohusky", "https://linkedin.com/joana-scheer"];
-
-var coluna2 = document.getElementById("icons");
-for (var i = 0; i < imagens.length; i++) {
-    coluna2.innerHTML += `<a href="${links[i]}"><img src="${imagens[i]}" alt="Imagem ${i + 1}"></a>`;
-}
+document.addEventListener("DOMContentLoaded", function() {
+    var userName = "Woman in Tech";
+    var imagem = "images/fem.png";
+    var currentYear = new Date().getFullYear();
+    var link = "https://mackenzie.com.br";
+    var coluna1 = document.getElementById('coluna1');
+    coluna1.innerHTML = `<p>${userName} <img src="${imagem}" alt="sexo: Feminino"> * ${currentYear} | <a href="${link}">Mackenzie</a></p>`;
+});
 
 var imagensElementos = document.querySelectorAll("#team img");
 for (var i = 0; i < imagensElementos.length; i++) {
@@ -33,4 +26,13 @@ document.addEventListener("DOMContentLoaded", function() {
             document.getElementById('menu').innerHTML = data;
         })
         .catch(error => console.error('Erro ao carregar o menu:', error));
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    fetch('footer.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('footer').innerHTML = data;
+        })
+        .catch(error => console.error('Erro ao carregar o footer:', error));
 });
