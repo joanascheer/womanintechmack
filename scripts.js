f0f274aa653829d0a7adaabe3f8e7d5382ac1d46
Joana Scheer <scheer.joana@gmail.com>
2024-06-04 11:42:47 -0300

    Filter on team's img

--- scripts.js
+++ scripts.js
@@ -1,12 +1,11 @@
 
-var userName = "Joana Scheer Loayza";
+var userName = "Woman in Tech";
 var imagem = "images/fem.png";
 var currentYear = new Date().getFullYear();
 var link = "https://mackenzie.com.br";
-var polo = "Polo: Higienópolis";
 
 var coluna1 = document.getElementById('coluna1');
-coluna1.innerHTML = `<p>${userName} <img src="${imagem}" alt="sexo: Feminino"> * ${currentYear} | <a href="${link}">Mackenzie</a> ~ ${polo}</p>`;
+coluna1.innerHTML = `<p>${userName} <img src="${imagem}" alt="sexo: Feminino"> * ${currentYear} | <a href="${link}">Mackenzie</a></p>`;
 
 var imagens = ["images/fb.png", "images/ig.png", "images/in.png"];
 var links = ["https://fcebook.com/primecatsgatil", "https://instagram.com/gudan_ohusky", "https://linkedin.com/joana-scheer"];
@@ -16,72 +15,22 @@ for (var i = 0; i < imagens.length; i++) {
     coluna2.innerHTML += `<a href="${links[i]}"><img src="${imagens[i]}" alt="Imagem ${i + 1}"></a>`;
 }
 
-var imagensElementos = document.querySelectorAll("#coluna2 img");
+var imagensElementos = document.querySelectorAll("#team img");
 for (var i = 0; i < imagensElementos.length; i++) {
     imagensElementos[i].addEventListener('mouseover', function() {
-        this.style.filter = 'invert(0)';
+        this.style.filter = 'grayscale(0)';
     });
 
     imagensElementos[i].addEventListener('mouseout', function() {
-        this.style.filter = 'invert(100%)';
+        this.style.filter = 'grayscale(1)';
     });
 }
 
-// A7
-
-let teams = [
-    {
-        name: "Cloud9",
-        playersNumber: 5,
-        shieldImg: "images/teamshields/Cloud9.png",
-        sport: "CS2",
-        color: "Azul"
-    },
-    {
-        name: "Blackhawks",
-        playersNumber: 6,
-        shieldImg: "images/teamshields/blackhawks.png",
-        sport: "Hockey",
-        color: "Vermelho"
-    },
-    {
-        name: "Lakers",
-        playersNumber: 5,
-        shieldImg: "images/teamshields/lakers.png",
-        sport: "Basquete",
-        color: "Dourado"
-    }
-];
-
-function showTeams(color) {
-    let divTeams = document.getElementById('teams');
-    divTeams.innerHTML = '';
-
-    for (let team of teams) {
-        if (team.color === color) {
-            let card = document.createElement('div');
-            card.className = 'card';
-
-            let cardBody = document.createElement('div');
-            cardBody.className = 'card-body';
-
-            let h5 = document.createElement('h5');
-            h5.className = 'card-title';
-            h5.textContent = team.name;
-            cardBody.appendChild(h5);
-
-            let p = document.createElement('p');
-            p.className = 'card-text';
-            p.textContent = `Modalidade: ${team.sport}, Jogadores: ${team.playersNumber}`;
-            cardBody.appendChild(p);
-
-            let img = document.createElement('img');
-            img.className = 'card-img-top';
-            img.src = team.shieldImg;
-            card.appendChild(img);
-
-            card.appendChild(cardBody);
-            divTeams.appendChild(card);
-        }
-    }
-}
+document.addEventListener("DOMContentLoaded", function() {
+    fetch('menu.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('menu').innerHTML = data;
+        })
+        .catch(error => console.error('Erro ao carregar o menu:', error));
+});
